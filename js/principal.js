5dac201ec0b5e58618e418f81387a2b8dd0007b1
franklindeoliveira <franklindeoliveira@gmail.com>
2016-04-16 11:07:28 -0300

    salvando os cartões no servidor

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,14 @@
+var usuario = 'franklindeoliveira@gmail.com';
+$.getJSON('https://ceep.herokuapp.com/cartoes/carregar?callback=?',
+          {usuario, usuario},
+          function(res){
+            var cartoes = res.cartoes;
+            console.log(cartoes.length + ' carregados em ' + res.usuario);
+            cartoes.forEach(function(cartao) {
+              adicionaCartao(cartao.conteudo, '');
+            });
+          });
+
 // pega os botões
 var $botoes = document.querySelectorAll('.opcoesDoCartao-remove');
 
@@ -12,6 +23,49 @@ $novoCartaoSalvar.addEventListener('click', validaCartao);
 document.querySelector('#mudaLayout').addEventListener('click', mudaLayout);
 
 $('.novoCartao').submit(criaNovoCartao);
+
+$('#busca').on('input', buscaCartao);
+
+$('#sync').click(salvarCartoes);
+
+function salvarCartoes() {
+
+  $('#sync').removeClass('botaoSync--sincronizado');
+  $('#sync').addClass('botaoSync--esperando');
+
+  var cartoes = [];
+  $('.cartao').each(function() {
+    var cartao = {};
+    cartao.conteudo = $(this).find('.cartao-conteudo').html();
+    cartoes.push(cartao);
+  });
+  // escolha seu nome de usuário aqui
+  var mural = {
+    usuario: 'franklindeoliveira@gmail.com',
+    cartoes: cartoes
+  };
+
+  $.ajax({
+    url: 'https://ceep.herokuapp.com/cartoes/salvar',
+    method: 'POST',
+    data: mural,
+    success: function(res) {
+      $('#sync').addClass('botaoSync--sincronizado');
+      console.log(res.quantidade + ' cartões salvos em ' + res.usuario + '.');
+    },
+    error: function() {
+      $('#sync').addClass('botaoSync--deuRuim');
+      console.log('Não foi possível salvar o mural.');
+    },
+    complete: function() {
+      $('#sync').removeClass('botaoSync--esperando');
+    }
+  });
+};
+
+function decideTipoCartao(conteudo) {
+  var quebras = conteudo.split('<br>').length;
+}
 
 function mudaLayout() {
   // pega o elemento com a class="mural"
@@ -41,24 +95,35 @@ function removeCartao() {
 
 function criaNovoCartao(event) {
   var campoConteudo = $('.novoCartao-conteudo');
-  var conteudo = campoConteudo.val().trim();
+  var conteudo = campoConteudo.val().trim().replace(/\n/g, '<br>');
 
   if (conteudo) {
-
-    var numeroDoCartao = $('.cartao').length + 1;
-
-    // cria o botão de remove
-    var botaoRemove = $('<button>').addClass('opcoesDoCartao-remove').attr('data-ref', numeroDoCartao).text('Remover').click(removeCartao);
-
-    // cria a div de opções
-    var opcoes = $('<div>').addClass('opcoesDoCartao').append(botaoRemove);
-
-    var conteudoTag = $('<p>').addClass("cartao-conteudo").append(conteudo);
-    $('<div>').addClass('cartao').attr('id', 'cartao_' + numeroDoCartao).append(opcoes).append(conteudoTag).prependTo('.mural');
+    adicionaCartao(conteudo, '#00ff00');
   }
   campoConteudo.val("");
 
   event.preventDefault();
+};
+
+function adicionaCartao(conteudo, cor) {
+  var numeroDoCartao = $('.cartao').length + 1;
+
+  // cria o botão de remove
+  var botaoRemove = $('<button>').addClass('opcoesDoCartao-remove').attr('data-ref', numeroDoCartao).text('Remover').click(removeCartao);
+
+  // cria a div de opções
+  var opcoes = $('<div>').addClass('opcoesDoCartao').append(botaoRemove);
+
+  var tipoCartao = decideTipoCartao(conteudo);
+
+  var conteudoTag = $('<p>').addClass("cartao-conteudo").append(conteudo);
+  $('<div>').addClass('cartao')
+            .attr('id', 'cartao_' + numeroDoCartao)
+            .append(opcoes)
+            .append(conteudoTag)
+            .css('background-color', cor)
+            .prependTo('.mural');
+
 };
 
 function validaCartao(event) {
@@ -76,3 +141,33 @@ function validaCartao(event) {
     event.preventDefault();
   }
 };
+
+function buscaCartao() {
+  // Guarda o valor digitado, removendo espaços extras.
+  var busca = $(this).val().trim();
+
+  if (busca.length) {
+    $('.cartao').hide().filter(function() {
+      return $(this).find('.cartao-conteudo')
+                    .text()
+                    .match(new RegExp(busca, 'i'));
+    }).show();
+  } else {
+    $('.cartao').show();
+  }
+}
+
+
+
+// function getJSON(url, callback) {
+//   console.log('Carregando dados do servidor: ' + url);
+//   var cartoes = [
+//     {'conteudo': 'texto', 'cor': 'azul'},
+//     {'conteudo': 'texto', 'cor': 'azul'},
+//     {'conteudo': 'texto', 'cor': 'azul'}];
+//   callback(cartoes);
+// };
+//
+// getJSON('www.api.com.br/cartoes', function(cartoes) {
+//   console.log(cartoes);
+// });
